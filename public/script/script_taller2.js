cd5881d9a8c3cca18adb4e286b71d96175518c29
Maria Alejandra Renjifo <marialejarenjifo@hotmail.com>
2019-04-13 23:27:16 -0500

    Filtros maquetados

--- public/script/script_taller2.js
+++ public/script/script_taller2.js
@@ -1,8 +1,38 @@
 //Iconos Feather
 feather.replace()
+
+//------------Para scroll suave-------------
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".nav-fija__enlace--icono").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+
+
+
 //-----------Para efecto de Fade In-----------------------
 const sr = ScrollReveal();
-sr.reveal('h1,p',{
+sr.reveal('h1',{
     
     duration:2000,
     origin: 'top',
@@ -163,9 +193,5 @@ function recorrerBtn2(boton){
 
 botones.forEach(recorrerBtn);
 botones2.forEach(recorrerBtn2);
-
-
-
-
 
 
