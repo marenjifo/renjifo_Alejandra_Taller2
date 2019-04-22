f35c15de67e15560ac2cbc3e246be5a8ac05ebed
Maria Alejandra Renjifo <marialejarenjifo@hotmail.com>
2019-04-22 09:44:10 -0500

    Creacion de ruta dinamica producto

--- express_taller2.js
+++ express_taller2.js
@@ -14,6 +14,24 @@ app.engine('handlebars',exphbs());
 //Establecer handlebars como el motor de render
 app.set('view engine','handlebars');
 
+//Arreglo de productos
+var productos = [];
+productos.push({
+    miniatura1: 'banner-P1.jpeg',
+    miniatura2: 'banda-P1.jpeg',
+    miniatura3: 'frente-P1.jpeg',
+    modelo: 'SERIES 4',
+    nombre: 'Gold Aluminum Case with Pink Sand Sport Band',
+    precio: '399',
+    codigoColor: 'FCCAE9',
+    color: 'Coral',
+    banda: 'Sport',
+    descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quo in! Inventore cum fugit obcaecati repellat. Fuga optio enim repudiandae necessitatibus consectetur numquam reiciendis qui ducimus, minus doloribus! Illo, perferendis.',
+    feature1: 'F1',feature2: 'F2',feature3: 'F3',feature4: 'F4',feature5: 'F5',
+    content1: 'C1',content2: 'C2',content3: 'C3',content4: 'C4',content5: 'C5',
+    detalles: 'detalles-P1.png',
+});
+
 
 //Ruta inicial
 app.get('/',function(req,res){
@@ -30,16 +48,35 @@ app.get('/tienda', function(req, res) {
     res.render('tienda',contexto);
 });
 
-//Pestana
-app.get('/pestana', function(req, res) {
+//Ruta a la tienda
+app.get('/prueba2', function(req, res) {
     var contexto = {
        
     };
     res.render('pestana',contexto);
 });
 
+//ruta dinamica
+app.get('/tienda/:pestana', function(req, res) {
+    var contexto= null;
+   
+       productos.forEach(function(producto){
+           if(producto.precio == req.params.pestana){
+               contexto=producto;
+           }
+       });
+   
+       if(contexto == null){
+           res.send('Page not found: '+req.params.pestana);
+       }else{
+           res.render('pestana',contexto);
+       }
+   
+       console.log(req.params.pestana);
+       
+   });
 
-
+   
 
 
 
