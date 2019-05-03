3cc7532ed1d9049baacbb6fc73f760a1279f63b7
Maria Alejandra Renjifo <marialejarenjifo@hotmail.com>
2019-05-03 13:08:35 -0500

    Creacion de productos con base de datos

--- express_taller2.js
+++ express_taller2.js
@@ -7,27 +7,17 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName = 'tienda';
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useNewUrlParser: true });
+var clientdb=null;
 
 //Mongo: conectar (Paso 2)
 client.connect(function(err) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-    const clientdb = client.db(dbName);
-    const productos = clientdb.collection('productos');
-    
-    productos.find({}, { sort: ['precio'] }).toArray(function(err,docs){
-        assert.equal(null,err);
-        console.log('Encontrados los documentos');
-        docs.forEach(function(prod){
-            console.log(prod.precio);
+    clientdb = client.db(dbName);
 
-        });
-        
-
-    });
   
-    client.close();
+   // client.close();
   });
 
 //Crear app de express
@@ -44,38 +34,19 @@ app.set('view engine','handlebars');
 //Arreglo de productos
 var productos = [];
 productos.push({
-    numero: 'P1',
-    miniatura1: 'banner-P1.jpeg',
-    miniatura2: 'banda-P1.jpeg',
-    miniatura3: 'frente-P1.jpeg',
-    modelo: 'SERIES 4',
-    nombre: 'Pink Aluminum Case with Sport Band',
-    precio: '399',
-    codigoColor: 'FCCAE9',
-    color: 'Pink',
-    banda: 'Sport',
-    descripcion: 'Compared to the Apple Watch 3, the new Apple Watch 4 offers a faster processor (S4 64-bit dual-core processor), larger screen and louder speaker to go along with LTE cellular support, walkie-talkie mode, more extensive and context-based notifications, and more.',
-    feature1: 'Built-in GPS',feature2: 'W3 Apple wireless chip',feature3: 'Barometric altimeter',feature4: 'Capacity 16GB',feature5: 'Optical heart sensor',
-    content1: 'Gold Aluminum Case',content2: 'S/M Sport Band',content3: '1m Magnetic Charging Cable',content4: '5W USB Power Adapter',content5: 'Ambient light sensor',
-    detalles: 'detalles-P1.png',
+        numero: "P1",
+        imagenes: ["banner-P1.jpeg","banda-P1.jpg","frente-P1.jpeg","detalles-P1.png"],
+        modelo: "SERIES 4",
+        nombre: "Pink Aluminum Case with Sport Band",
+        precio: 399,
+        codigoColor: "FCCAE9",
+        color: "Pink",
+        banda: "Sport",
+        descripcion: "Compared to the Apple Watch 3, the new Apple Watch 4 offers a faster processor (S4 64-bit dual-core processor), larger screen and louder speaker to go along with LTE cellular support, walkie-talkie mode, more extensive and context-based notifications, and more.",
+        caracteristicas: ["Built-in GPS","W3 Apple wireless chip","Barometric altimeter","Capacity 16GB","Optical heart sensor"],
+        contenido: ["Pink Aluminum Case","S/M Sport Band","1m Magnetic Charging Cable","5W USB Power Adapter","Ambient light sensor"]
 });
 
-productos.push({
-    numero: 'P2',
-    miniatura1: 'banner-P1.jpeg',
-    miniatura2: 'banda-P1.jpeg',
-    miniatura3: 'frente-P1.jpeg',
-    modelo: 'NIKE +',
-    nombre: 'Silver Aluminum Case with Sport Band',
-    precio: '599',
-    codigoColor: 'E8E8E8',
-    color: 'Silver',
-    banda: 'Sport',
-    descripcion: 'Compared to the Apple Watch 3, the new Apple Watch 4 offers a faster processor (S4 64-bit dual-core processor), larger screen and louder speaker to go along with LTE cellular support, walkie-talkie mode, more extensive and context-based notifications, and more.',
-    feature1: 'Built-in GPS',feature2: 'W3 Apple wireless chip',feature3: 'Barometric altimeter',feature4: 'Capacity 16GB',feature5: 'Optical heart sensor',
-    content1: 'Gold Aluminum Case',content2: 'S/M Sport Band',content3: '1m Magnetic Charging Cable',content4: '5W USB Power Adapter',content5: 'Ambient light sensor',
-    detalles: 'detalles-P1.png',
-});
 
 
 //Ruta inicial
@@ -87,10 +58,26 @@ app.get('/',function(req,res){
 
 //Ruta a la tienda
 app.get('/tienda', function(req, res) {
-    var contexto = {
+
+    //Mongo: buscar documentos (Paso 3)
+    const productos = clientdb.collection('productos');
+    productos.find({}).toArray(function(err,docs){
+        assert.equal(null,err);
+       /* console.log('Encontrados los documentos');
+        docs.forEach(function(prod){
+            console.log(prod.nombre);
+        });*/
+
+        var contexto = {
+            listaProductos: docs
+        };
+        res.render('tienda',contexto);
+    });
+
+   /* var contexto = {
         listaProductos: productos
-    };
-    res.render('tienda',contexto);
+    };*/
+   
 });
 
 //Ruta al carrito
@@ -106,20 +93,26 @@ app.get('/carrito', function(req, res) {
 //ruta dinamica
 app.get('/tienda/:pestana', function(req, res) {
     var contexto= null;
+
+    const productos = clientdb.collection('productos');
+    productos.find({}).toArray(function(err,docs){
+        assert.equal(null,err);
+      
+        docs.forEach(function(prod){
+            if(prod.numero == req.params.pestana){
+                contexto=prod;
+            }
+        });
+
+       
+        if(contexto == null){
+            res.send('Page not found: '+req.params.pestana);
+        }else{
+            res.render('pestana',contexto);
+        }
+    });
    
-       productos.forEach(function(producto){
-           if(producto.numero == req.params.pestana){
-               contexto=producto;
-           }
-       });
-   
-       if(contexto == null){
-           res.send('Page not found: '+req.params.pestana);
-       }else{
-           res.render('pestana',contexto);
-       }
-   
-       console.log(req.params.pestana);
+      
        
    });
 
